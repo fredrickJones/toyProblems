4267cd77d5afafa2cf7cb691f5be328992655076
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-02-03 20:44:21 -0700

    updated

---
+++ 3Feb2015.js
@@ -0,0 +1,12 @@
+'use strict';
+// create a function that takes in 3 strings as parameters. the goal is to swap all x characters in the string for y characters.
+var swapper = function(str, char1, char2) {
+	var stringArr = str.split('');
+	for (var i = 0; i < stringArr.length; i++) {
+		if (stringArr[i] === char1) {
+			stringArr[i] === char2;
+		}
+		var string = arr.join('');
+		return string;
+	};
+};
